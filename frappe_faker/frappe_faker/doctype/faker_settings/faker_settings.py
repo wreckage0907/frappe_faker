533b9ff20b978c03a48eedea1c57de214736c289
wreckage0907 <girish.raghav2004@gmail.com>
2026-04-11 15:54:57 +0530

    chore: create settings doctype

---
+++ frappe_faker/frappe_faker/doctype/faker_settings/faker_settings.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2026, wreckage0907 and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FakerSettings(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		ai_provider: DF.Literal["", "OpenAI", "Anthropic", "Ollama", "Custom"]
+		api_endpoint: DF.Data | None
+		api_key: DF.Password | None
+		default_count: DF.Int
+		model_name: DF.Data | None
+	# end: auto-generated types
+
+	pass
